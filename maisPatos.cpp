2b77e62303b85ce70a4228473d2e033ae6ca9030
Débora Castro <107296659+cyberdebb@users.noreply.github.com>
2023-07-22 02:43:42 -0300

    Update maisPatos.cpp

--- maisPatos.cpp
+++ maisPatos.cpp
@@ -1,21 +1,21 @@
-//#include <opencv2/highgui.hpp>
-//#include <opencv2/imgproc.hpp>
-//
-//using namespace cv;
-//
-//int main()
-//{
-//	Mat img = imread(R"(C:\Users\debor\OneDrive\Documentos\imagensOpenCV\pato.jpg)", IMREAD_REDUCED_COLOR_2);
-//	Mat eroded, dilated;
-//
-//	Mat elementKernel = getStructuringElement(MORPH_RECT, Size(10, 10), Point(-1, -1));
-//	erode(img, eroded, elementKernel, Point(-1, -1), 1);
-//	dilate(img, dilated, elementKernel, Point(-1, -1), 1);
-//
-//	imshow("erodido", eroded); //eliminate thin lines
-//	imshow("dilatada", dilated); //thin lines become thicker
-//	imshow("default", img);
-//	waitKey();
-//
-//	return 0;
-//}
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+using namespace cv;
+
+int main()
+{
+	Mat img = imread(R"(C:\Users\debor\OneDrive\Documentos\imagensOpenCV\pato.jpg)", IMREAD_REDUCED_COLOR_2);
+	Mat eroded, dilated;
+
+	Mat elementKernel = getStructuringElement(MORPH_RECT, Size(10, 10), Point(-1, -1));
+	erode(img, eroded, elementKernel, Point(-1, -1), 1);
+	dilate(img, dilated, elementKernel, Point(-1, -1), 1);
+
+	imshow("erodido", eroded); //eliminate thin lines
+	imshow("dilatada", dilated); //thin lines become thicker
+	imshow("default", img);
+	waitKey();
+
+	return 0;
+}
