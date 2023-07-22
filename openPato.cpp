cd47beec03230fc1927bfa0c5ee566953d66ec53
Débora Castro <107296659+cyberdebb@users.noreply.github.com>
2023-07-22 02:44:02 -0300

    Update openPato.cpp

--- openPato.cpp
+++ openPato.cpp
@@ -1,13 +1,13 @@
-//#include <opencv2/highgui.hpp>
-//
-////using namespace cv;
-//
-//int main()
-//{
-//    cv::Mat img = cv::imread(R"(C:\Users\debor\OneDrive\Documentos\imagensOpenCV\pato.jpg)");
-//    cv::namedWindow("olha esse pato", cv::WINDOW_AUTOSIZE);
-//    cv::imshow("olha esse pato", img);
-//    cv::moveWindow("olha esse pato", 0, 45);
-//    cv::waitKey(0);
-//    return 0;
-//}
+#include <opencv2/highgui.hpp>
+
+//using namespace cv;
+
+int main()
+{
+   cv::Mat img = cv::imread(R"(C:\Users\debor\OneDrive\Documentos\imagensOpenCV\pato.jpg)");
+   cv::namedWindow("olha esse pato", cv::WINDOW_AUTOSIZE);
+   cv::imshow("olha esse pato", img);
+   cv::moveWindow("olha esse pato", 0, 45);
+   cv::waitKey(0);
+   return 0;
+}
